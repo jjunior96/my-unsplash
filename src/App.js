3ee3c3654cd50beee77c64e5cfef1665e648b258
Junior <jjalves96@gmail.com>
2020-09-03 16:34:32 -0300

    feat: base project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './App.css';
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    api.get(`photos/?client_id=${process.env.REACT_APP_CLIENT_ID}`).then(response => {
+      setPhotos(response.data) ;
+    });
+
+  }, []);
+
+
   return (
-    <h1>Teste</h1>
+    <div>
+      <ul className="photos-list" >
+      {photos.map(photo => {
+          return (
+            <li key={photo.id} >
+              <a href={photo.urls.raw} target="_blank" rel="noopener noreferrer">
+                <img src={photo.urls.raw} alt={photo.description}/>
+              </a>
+            </li>  
+          )
+        
+      })}
+      </ul>
+    </div>
   );
 }
 
